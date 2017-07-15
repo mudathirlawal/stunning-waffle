b6c4b0bc38870e6e9ca7a66056cc9f1588fbc285
MUDATHIR LAWAL <al-mudathir@live.com>
2017-07-15 09:55:07 +0100

    My PHP Assignment 15-JULY-2017

--- productTable.php
+++ productTable.php
@@ -1,8 +1,14 @@
 <?php
-    
-    $count = 1;
-    $array{12} = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12};
-
-    for($count = 1, $count <= 12, $count++) {
-    	print_r ($array * $count);
-    }
+	<const NUM = 2;
+	    $count = 1; 
+	    $product = NUM * $count;
+	    $array = [1, 2, 3, 4, 5,  6, 7, 8, 9, 10, 11, 12];
+	   
+	    for($count = 1; $count <= 12; $count++ ) {
+	      print_r(NUM."  X  $count =  $product\n");
+	    }
+        
+        # Program Name : Multiplication Table
+        # I couldn't print the correct results.
+	    # Kindly assit, thanks.
+		
